78b844f2ed20e2bf9d5c5cdc31180ea63faea98a
Maltsev Nikita <maltsew.na@gmail.com>
2023-02-06 18:02:00 +0300

    Добавил модели приложения блог
    Модель профиля пользователя - основанная на User, с отношением 1 к 1
    Модель постов, выполняющая основное назначение блога - публикации

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+''' В качестве модели пользователя используется models.User'''
+
+
+# Модель профиля пользователя
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Профиль пользователя')
+    # profile_image = models.ImageField(default='default.jpeg', upload_to='profile_pics', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    class Meta:
+        verbose_name_plural = 'Профили'
+        verbose_name = 'Профиль'
+
+
+# Модель "пост" - основной тип публикуемой информации в блоге
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название поста')
+    slug = models.SlugField(max_length=200, unique=True, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    updated_on = models.DateTimeField(auto_now=True, verbose_name='Дата обновления поста')
+    content = models.TextField(verbose_name='Содержание поста')
+    categories = models.CharField(max_length=40, verbose_name='Категория поста')
+    created_on = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания поста')
+    #image = models.ImageField(null=True, blank=True, upload_to='images/')
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
+
+    def __str__(self):
+        return self.title
