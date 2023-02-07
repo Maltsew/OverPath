6a70a3ed9033881dae6caf4e63c4179014758111
Maltsev Nikita <maltsew.na@gmail.com>
2023-02-07 18:03:02 +0300

    Зарегистрированы модели Пост, Профиль и Категория в админке

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from blog.models import Profile, Post
+from blog.models import Profile, Post, Category
+
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(Post)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    # Последовательность имен полей, которые будут преобразованы в гиперссылки на страницы правки записи
+    list_display_links = ('user',)
+    search_fields = ('user',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle')
+    list_display_links = ('title',)
+    search_fields = ('title', 'subtitle')
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on',)
+    list_display_links = ('title',)
+    search_fields = ('title', 'author', 'created_on')
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
