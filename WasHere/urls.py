5451e22e1d2152e383937b8e9cefdf26a7608e3a
Maltsev Nikita <maltsew.na@gmail.com>
2023-02-09 13:44:32 +0300

    указал Media URL и ROOT в настройках проекта; в режиме отладки - возможность работы со статич. файлами

--- WasHere/urls.py
+++ WasHere/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 import blog.views
+from WasHere import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
 ]
+# Настройка просмотра статических медиа при работе в отладке
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = blog.views.pageNotFound
+
