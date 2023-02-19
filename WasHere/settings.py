50789bc7403ed6150b5b004ea7a1a459171c5400
Maltsev Nikita <maltsew.na@gmail.com>
2023-02-19 15:44:53 +0300

    Развилка для одновременной работы под linux и macos Настройки статических файлов

--- WasHere/settings.py
+++ WasHere/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from sys import platform
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,16 +77,20 @@ WSGI_APPLICATION = 'WasHere.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogwashere',
-        'USER': 'bloguser',
-        'PASSWORD': 'ubuntu',
-        'HOST': 'localhost',
-        'PORT': '',
+
+if platform == 'darwin':
+    pass
+elif platform == "linux" or platform == "linux2":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'blogwashere',
+            'USER': 'bloguser',
+            'PASSWORD': 'ubuntu',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
 
 
 # Password validation
@@ -121,10 +127,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+'django.contrib.staticfiles.finders.FileSystemFinder',
+)
+
 # нестандартные пути хранения статических файлов (уровня приложения)
-STATICFILES_DIRS = []
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
