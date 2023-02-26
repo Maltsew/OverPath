af958930bf878830f4be2df7b3649b9641ebdc82
Maltsev Nikita <maltsew.na@gmail.com>
2023-02-26 23:02:19 +0300

    Заготовка формы для создания поста

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,25 @@
 # Форма, связанная с моделью
 from django.forms import ModelForm
-from blog.models import Category, Profile
 
+from django import forms
+from .models import Post, Images, Category
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content',]
+
+
+class ImageForm(forms.ModelForm):
+    image = forms.FileField(label='Image')
+
+    class Meta:
+        model = Images
+        fields = ('image',)
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['title', 'subtitle',]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from blog.models import Post, Category
+from blog.models import Post, Category, Images, Profile
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.http import HttpResponseNotFound, Http404, HttpResponse
 
+from django.forms import modelformset_factory
+from .forms import ImageForm, PostForm, CategoryForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
 
 def homepage(request):
-    posts = Post.objects.all()
-    categories = Category.objects.all()
     context = {
-        'posts': posts,
-        'categories': categories,
     }
     return render(request, 'blog/homepage.html', context=context)
 
@@ -27,22 +30,11 @@ def about(request):
 def categories(request):
     # всего категорий
     categories_count = Category.objects.all().count()
-    # множество всех категорий
-    categories = Category.objects.all()
-
     context = {
         'title': 'Все категории',
         'categories_count': categories_count,
-        'categories': categories,
     }
     return render(request, 'blog/categories.html', context=context)
-
-
-def add_post(request):
-    context = {
-
-    }
-    return HttpResponse("Добавить пост")
 
 
 def login(request):
@@ -54,12 +46,10 @@ def login(request):
 
 def show_post(request, post_id):
     post = Post.objects.filter(id=post_id)
-    #new = post.values_list('title', 'author_id', 'author', 'content',)
     context = {
         'post': post,
     }
     return render(request, 'blog/post.html', context=context)
-    # return HttpResponse(f"Отображение поста с id = {post_id}")
 
 
 def show_category(request, cat_id):
@@ -71,6 +61,10 @@ def show_category(request, cat_id):
         'current_cat': current_cat,
     }
     return render(request, 'blog/posts_by_category.html', context=context)
+
+
+def add_post(request):
+    pass
 
 
 def pagenotfound(request, exception):
