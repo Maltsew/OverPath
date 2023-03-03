e64e6975a1473a6b2cf81d4015fd9900122a7393
Malcev Nikita <maltsew.na@gmail.com>
2023-03-03 13:56:09 +0300

    В шаблон добавления поста добали обработчики ошибок валидации формы Добавил обработку кастомного валидатора: Поскольку модель поста предусматривает UniqueConstraint для полей "название" и "контент", валидатор поднимет ошибку валидации на форме с сообщением о дублировании названия и контента

--- blog/forms.py
+++ blog/forms.py
@@ -8,9 +8,19 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags', 'preview_image', 'images', ]
+        fields = ['title', 'author', 'content', 'tags', 'preview_image', 'images', 'slug']
         widgets = {
             'title': forms.TextInput(attrs={'size': 80}),
             'content': forms.Textarea(attrs={'cols': 79, 'rows': 20}),
-            'tags': forms.Select(),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data['title']
+        content = cleaned_data['content']
+        if title and Post.objects.get(title=title):
+            if content and Post.objects.get(content=content):
+                msg = 'Такой пост уже существует'
+                self.add_error('title', msg)
+                raise forms.ValidationError(("Такой пост уже существует"), code='invalid')
+        return cleaned_data
