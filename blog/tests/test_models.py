5ad25d02dd5229e00799b884b2a119b236acee6b
Maltsev Nikita <maltsew.na@gmail.com>
2023-04-15 16:22:08 +0300

    Добавлены тесты модели Post

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from blog.models import Profile, Tag
+from blog.models import Profile, Tag, Post
 from django.contrib.auth.models import User
 
 from django.db import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 from unittest import skip
 
-from blog.views import BlogTags
-
+from transliterate import translit, get_available_language_codes
+from django.utils.text import slugify
 
 """ Тесты для моделей приложения blog
 !WARNING!
@@ -78,6 +78,7 @@ class ProfileModelTests(TestCase):
         profile = Profile.objects.get(user=user)
         self.assertEqual(profile._meta.verbose_name_plural.title(), 'Профили')
 
+
 class TagModelTests(TestCase):
     """ Тесты для модели Tag"""
 
@@ -88,7 +89,7 @@ class TagModelTests(TestCase):
         сохраняем созданную запись в качестве переменной класса"""
         cls.tag = Tag.objects.create(title='Тест', slug='test')
 
-    def test_tag_represent(self):
+    def test_tag_representation(self):
         """ __str__ возвращает название тэга"""
         tag = TagModelTests.tag
         self.assertEqual(tag.title, str(tag))
@@ -109,3 +110,53 @@ class TagModelTests(TestCase):
         """ Полученное из Meta модели Tag verbose_name_plural"""
         tag = TagModelTests.tag
         self.assertEqual(tag._meta.verbose_name_plural.title(), 'Тэги')
+
+
+class PostModelTests(TestCase):
+    """ Тесты для модели Post"""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        """ Создаем тестовую запись в БД
+        сохраняем созданную запись в качестве переменной класса
+        Примечание: поскольку в модели используется автор как внешний ключ к User, и поле тэгов со связью многие со многими,
+        для тестирования функционирования поста необходимо создать и автора и тэг"""
+        cls.tag = Tag.objects.create(title='Тест', slug='test')
+        cls.user = User.objects.create(username='IvanovIvan', first_name='Ivan', last_name='Ivanov',
+                                       email='ivanov@gmail.com')
+        cls.post = Post.objects.create(title='Test Title', author=cls.user, content='Test post content')
+        cls.post.tags.add(cls.tag)
+
+    def test_save_create_post_slug(self):
+        """ слаг поста, определяемый методом save(), добавляется при создании поста
+        в соответствии с правилами создания слага"""
+        post = PostModelTests.post
+        expected_post_slug = 'test-title'
+        self.assertEqual(expected_post_slug, post.slug)
+
+    def test_post_slug_len(self):
+        """ слаг поста ограничен 100 первыми символами названия"""
+        # создадим новый пост, название которого будет больше 100 символов
+        user = PostModelTests.user
+        very_long_string = 20*'string'
+        post = Post.objects.create(title=very_long_string, author=user, content='Test post content')
+        self.assertEqual(100, len(post.slug))
+
+    def test_post_representation(self):
+        """__str__ возвращает название поста"""
+        post = PostModelTests.post
+        self.assertEqual(post.title, str(post))
+
+    def test_unique_title_content_constraints(self):
+        """ Запрет создания поста с существующими в БД title и content"""
+        post = PostModelTests.post
+        # создание нового тэга
+        user = User.objects.create(username='PetrovPent', first_name='Petr', last_name='Petrov',
+                                       email='petrov@gmail.com')
+        # создание полностью идентичного поста как в setUp
+        # тэг на данном этапе не критичен
+        try:
+            post = Post.objects.create(title='Test Title', author=user, content='Test post content')
+        except IntegrityError:
+            print('Test passed! Can not duplicate post!')
