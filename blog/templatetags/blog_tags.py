b66a88c0b796bfd75adbc821d439be5d5b1dea6a
Maltsev Nikita <maltsew.na@gmail.com>
2023-05-03 18:22:16 +0300

    Добавлено отображение самых популярных тэгов Популярность определяется количеством использований данного тэга в постах

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Tag, Post
+from django.db.models import Max
 
 
 register = template.Library()
@@ -25,3 +26,21 @@ def get_all_tags():
 @register.simple_tag()
 def get_tags_count():
     return Tag.objects.all().count()
+
+
+@register.simple_tag()
+def get_tag_uses_counter():
+    t = Tag.objects.all()
+    # если тэгов пока нет, вернет пустой словарь для обработки исключения,
+    # тогда в популярных тэгах следует выводить сообщение
+    if len(t) == 0:
+        return {}
+    tags_dict = {}
+    # для каждого тэга найдем количество раз, когда он используется
+    for tag in t:
+        counter = tag.post_set.all().count()
+        tags_dict[tag] = counter
+    # отсортируем в словаре 3 самых популярных поста
+    tag_counter = sorted(tags_dict.items(), key=lambda item: item[1], reverse=True)
+    # возвращает пары тэг:количество_использований
+    return dict(tag_counter[:3])
